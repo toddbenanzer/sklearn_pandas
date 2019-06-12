8ad1d0a8b90b25caadc4bcae9aeed3664efabdd7
toddbenanzer <toddbenanzer@gmail.com>
2019-06-11 21:30:28 -0400

    add function apply

--- sklearn_pandas/base.py
+++ sklearn_pandas/base.py
@@ -22,13 +22,45 @@ class DataFrameFeatureUnion(BaseEstimator, TransformerMixin):
 
 class DataFrameModelTransformer(TransformerMixin):
 
-    def __init__(self, model, output_column_names):
+    def __init__(self, model, output_column_names=None):
         self.model = model
         self.output_column_names = output_column_names
 
+    def _validate_params(self):
+        if self.output_column_names is None:
+            self.output_column_names = ['model_out']
+        elif type(self.output_column_names) == str:
+            self.output_column_names = [self.output_column_names, ]
+
     def fit(self, *args, **kwargs):
+        self._validate_params()
         self.model.fit(*args, **kwargs)
         return self
 
     def transform(self, X, **transform_params):
-        return pd.DataFrame(self.model.predict(X), columns=self.output_column_names)
+        y = self.model.predict(X)
+        return pd.DataFrame(y, columns=self.output_column_names)
+
+
+class DataFrameFunctionApply(BaseEstimator, TransformerMixin):
+
+    def __init__(self, func=None, prefix='', suffix=''):
+        self.func = func
+        self.prefix = prefix
+        self.suffix = suffix
+
+    def _validate_params(self):
+        if self.func is None:
+            self.func = lambda x: x
+
+    def fit(self, X, y=None):
+        self._validate_params()
+        return self
+
+    def transform(self, X):
+        new_col_list = []
+        for col in X.columns:
+            new_col_name = self.prefix + col + self.suffix
+            new_col_list.append(new_col_name)
+            X[new_col_name] = X[col].map(self.func)
+        return X.loc[:, new_col_list]
---
+++ sklearn_pandas/tests/test_Base.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+import pandas as pd
+from sklearn.dummy import DummyRegressor
+from sklearn_pandas.base import DataFrameModelTransformer, DataFrameFunctionApply
+
+
+class TestModelTransformer(TestCase):
+    def test_transform(self):
+        df = pd.DataFrame({'A': [1, 2, ]})
+        expected_out = pd.DataFrame({'B': [0.0, 0.0, ]})
+        y = [0.0, 3.0, ]
+        model = DummyRegressor(strategy='constant', constant=0.0)
+        model_transform = DataFrameModelTransformer(model, output_column_names=['B'])
+        model_transform.fit(df, y)
+        model_output = model_transform.transform(df)
+        pd.testing.assert_frame_equal(expected_out, model_output)
+
+
+class TestDataFrameFunctionApply(TestCase):
+    def test_identity(self):
+        df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+        dffa = DataFrameFunctionApply()
+        expected_out = df
+        pd.testing.assert_frame_equal(expected_out, dffa.fit_transform(df))
+
+    def test_plus_one_prefix(self):
+        df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+        dffa = DataFrameFunctionApply(func=lambda x: 2*x, prefix='dub_')
+        expected_out = pd.DataFrame({'dub_A': [2, 4, 6], 'dub_B': [8, 10, 12]})
+        pd.testing.assert_frame_equal(expected_out, dffa.fit_transform(df))
+
+    def test_plus_one_suffix(self):
+        df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+        dffa = DataFrameFunctionApply(func=lambda x: 2*x, suffix='_dub')
+        expected_out = pd.DataFrame({'A_dub': [2, 4, 6], 'B_dub': [8, 10, 12]})
+        pd.testing.assert_frame_equal(expected_out, dffa.fit_transform(df))
