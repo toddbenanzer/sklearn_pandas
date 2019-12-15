d5fab42d3e4f9fea8b64db2a8f1c77c349d253e7
toddbenanzer <toddbenanzer@gmail.com>
2019-12-15 12:43:54 -0500

    force transform copy

--- sklearn_pandas/tests/test_feature_selection.py
+++ sklearn_pandas/tests/test_feature_selection.py
@@ -3,19 +3,97 @@ from sklearn_pandas.transformers.feature_selection import *
 
 
 class TestPandasSelectKBest(TestCase):
-
-    def test_continuous_input(self):
+    def get_Xy_simple(self):
         X = pd.DataFrame({
             'A': [1.0, 1.0, 1.0, 1.0, 2.0, ],
             'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
             'C': [1.0, 2.0, 3.0, 3.0, 5.0, ],
             'D': [1.0, 2.0, 2.0, 2.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
         })
         y = [1.0, 2.0, 3.0, 4.0, 5.0, ]
+        return X, y
 
+    def test_continuous_input(self):
+        X, y = self.get_Xy_simple()
+        expected_df = pd.DataFrame({
+            'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
+        })
+        transform = PandasSelectKBest()
+        out_df = transform.fit_transform(X, y)
+        pd.testing.assert_frame_equal(out_df, expected_df)
+
+    def test_continuous_input_k3(self):
+        X, y = self.get_Xy_simple()
         expected_df = pd.DataFrame({
             'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
             'C': [1.0, 2.0, 3.0, 3.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
         })
         transform = PandasSelectKBest(k=3)
-        pd.testing.assert_frame_equal(transform.fit_transform(X, y), expected_df)
+        out_df = transform.fit_transform(X, y)
+        pd.testing.assert_frame_equal(out_df, expected_df)
+
+    def test_continuous_input_pctk(self):
+        X, y = self.get_Xy_simple()
+        expected_df = pd.DataFrame({
+            'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
+            'C': [1.0, 2.0, 3.0, 3.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
+        })
+        transform = PandasSelectKBest(k=0.5)
+        out_df = transform.fit_transform(X, y)
+        pd.testing.assert_frame_equal(out_df, expected_df)
+
+    def test_continuous_input_all(self):
+        X, y = self.get_Xy_simple()
+        expected_df = X
+        transform = PandasSelectKBest(k='all')
+        out_df = transform.fit_transform(X, y)
+        pd.testing.assert_frame_equal(out_df, expected_df)
+
+    def test_continuous_input_pearson(self):
+        X, y = self.get_Xy_simple()
+        expected_df = pd.DataFrame({
+            'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
+        })
+        transform = PandasSelectKBest(method='pearson')
+        out_df = transform.fit_transform(X, y)
+        pd.testing.assert_frame_equal(out_df, expected_df)
+
+    def test_continuous_input_spearman(self):
+        X, y = self.get_Xy_simple()
+        expected_df = pd.DataFrame({
+            'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
+            'C': [1.0, 2.0, 3.0, 3.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
+        })
+        transform = PandasSelectKBest(method='spearman')
+        out_df = transform.fit_transform(X, y)
+        pd.testing.assert_frame_equal(out_df, expected_df)
+
+
+class TestPandasSelectThreshold(TestCase):
+    def get_Xy_simple(self):
+        X = pd.DataFrame({
+            'A': [1.0, 1.0, 1.0, 1.0, 2.0, ],
+            'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
+            'C': [1.0, 2.0, 3.0, 3.0, 5.0, ],
+            'D': [1.0, 2.0, 2.0, 2.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
+        })
+        y = [1.0, 2.0, 3.0, 4.0, 5.0, ]
+        return X, y
+
+    def test_continuous_input(self):
+        X, y = self.get_Xy_simple()
+        expected_df = pd.DataFrame({
+            'B': [1.0, 2.0, 3.0, 4.0, 5.0, ],
+            'E': [-1.0, -1.0, -3.0, -4.0, -5.0, ],
+        })
+        transform = PandasSelectThreshold(pct=0.20)
+        out_df = transform.fit_transform(X, y)
+        print(out_df)
+        pd.testing.assert_frame_equal(out_df, expected_df)
--- sklearn_pandas/transformers/feature_selection.py
+++ sklearn_pandas/transformers/feature_selection.py
@@ -1,22 +1,92 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, f_oneway, spearmanr, kendalltau
 from sklearn_pandas.util import retain_sign, validate_dataframe
 from sklearn.base import BaseEstimator, TransformerMixin, clone
 
 
 class PandasSelectKBest(BaseEstimator, TransformerMixin):
-    def __init__(self, k=2):
+    def __init__(self, k=2, method='pearson'):
         self.k = k
+        self.method = method
+
+    def _check_params(self, X, y):
+        num_cols = len(X.columns)
+        if type(self.k) == float:
+            self._k = int(np.ceil(num_cols * self.k))
+        elif (type(self.k) == str) & (self.k == 'all'):
+            self._k = num_cols
+        if type(self.k) == int:
+            self._k = self.k
+        self._k = min(self._k, num_cols)
+        self._k = max(self._k, 1)
+
+    def calc_rank(self, X, y):
+        if self.method == 'ftest':
+            corr = 1.0 * X.apply(func=lambda x, y: f_oneway(x, y)[0], axis=0, args=(y,)).abs()
+            return corr.rank(method='min')
+
+        elif self.method == 'pearson':
+            corr = -1.0 * X.apply(func=lambda x, y: pearsonr(x, y)[0], axis=0, args=(y,)).abs()
+            return corr.rank(method='min')
+
+        elif self.method == 'spearman':
+            corr = -1.0 * X.apply(func=lambda x, y:spearmanr(x, y)[0], axis=0, args=(y,)).abs()
+            return corr.rank(method='min')
+
+        elif self.method == 'spearman':
+            corr = -1.0 * X.apply(func=lambda x, y: kendalltau(x, y)[0], axis=0, args=(y,)).abs()
+            return corr.rank(method='min')
 
     def fit(self, X, y):
         X = validate_dataframe(X)
-        imp = {}
-        for col in X.columns:
-
+        self._check_params(X, y)
+        self.rank = self.calc_rank(X, y)
+        self.selected_columns = self.rank[self.rank <= self._k].index.tolist()
         return self
 
     def transform(self, X):
         X = validate_dataframe(X)
         X = X.copy()
-        return X
+        return X.loc[:, self.selected_columns]
+
+
+class PandasSelectThreshold(BaseEstimator, TransformerMixin):
+    def __init__(self, pct=0.05, method='pearson'):
+        self.pct = pct
+        self.method = method
+
+    def _check_params(self, X, y):
+        num_cols = len(X.columns)
+        self._pct = self.pct
+        self._pct = min(self._pct, 1.0)
+        self._pct = max(self._pct, 0.0)
+
+    def calc_imp(self, X, y):
+        if self.method == 'ftest':
+            corr = 1.0 * X.apply(func=lambda x, y: f_oneway(x, y)[0], axis=0, args=(y,)).abs()
+            return corr / corr.sum()
+
+        elif self.method == 'pearson':
+            corr = -1.0 * X.apply(func=lambda x, y: pearsonr(x, y)[0], axis=0, args=(y,)).abs()
+            return corr / corr.sum()
+
+        elif self.method == 'spearman':
+            corr = -1.0 * X.apply(func=lambda x, y:spearmanr(x, y)[0], axis=0, args=(y,)).abs()
+            return corr / corr.sum()
+
+        elif self.method == 'spearman':
+            corr = -1.0 * X.apply(func=lambda x, y: kendalltau(x, y)[0], axis=0, args=(y,)).abs()
+            return corr / corr.sum()
+
+    def fit(self, X, y):
+        X = validate_dataframe(X)
+        self._check_params(X, y)
+        self.imp = self.calc_imp(X, y)
+        self.selected_columns = self.imp[self.imp <= self._pct].index.tolist()
+        return self
+
+    def transform(self, X):
+        X = validate_dataframe(X)
+        X = X.copy()
+        return X.loc[:, self.selected_columns]
